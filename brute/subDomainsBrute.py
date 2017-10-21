1183c6e8cc6cd8a51857dbacf1e830f1a17d4d02
bit4 <bit4woo@163.com>
2017-10-21 17:15:13 +0800

    Revert "fix bug"
    This reverts commit ae7c0e051171c58e407283ca01a736d61eac1435.

--- brute/subDomainsBrute.py
+++ brute/subDomainsBrute.py
@@ -16,9 +16,7 @@ import re
 import dns.resolver
 import time
 import os
-print sys.path
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-print sys.path
+#sys.path.append("..")
 from lib.consle_width import getTerminalSize
 import config
 
